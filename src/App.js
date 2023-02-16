9e3bea121f500f7b146b81057843f4ca91683ecb
Kristopher Ong <k.ong@arcanys.com>
2023-02-16 21:06:41 +0800

    Add NewExpense component, ExpenseForm.js and ExpenseForm.css files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
   const expenses = [
     {
@@ -25,6 +25,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );

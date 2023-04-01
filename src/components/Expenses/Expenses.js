f4dd0df58b262b5e5a4ae19f0828d11c5587bc7e
Kristopher Ong <k.ong@arcanys.com>
2023-04-02 03:42:17 +0800

    ✨feature: 67. Adding Conditional Return Statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2023');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
-        console.log(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
     return (
-        <div>
+        <li>
             <Card className="expenses">
-                <ExpensesFilter selectedYear={filteredYear} onfilterChange={filterChangeHandler}/>
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+                <ExpensesFilter
+                    selectedYear={filteredYear} 
+                    onfilterChange={filterChangeHandler}/>
+                <ExpensesList items={filteredExpenses}/>
             </Card>
-        </div>
+        </li>
         
     )
 }
